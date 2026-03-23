3b61720fe9975849a86bbc875a236b6425c6342d
Kevin <kevin.montes03@uceva.edu.co>
2026-03-22 19:08:52 -0500

    Creacion de Model y Repository

---
+++ build-check-backend/src/main/java/co/edu/uceva/buildcheck/modules/movimientos/model/Movimiento.java
@@ -0,0 +1,38 @@
+package main.java.co.edu.uceva.buildcheck.modules.movimientos.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "movimientos")
+public class Movimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "El tipo de movimiento no puede estar vacio")
+    @Column(nullable = false)
+    private String tipoMovimiento; // ENTRADA o SALIDA
+
+    @NotNull(message = "La cantidad es obligatoria")
+    @DecimalMin(value = "0.1", message = "La cantidad debe ser mayor a 0")
+    private Double cantidad;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fecha;
+
+    // Relación con Material
+    @ManyToOne
+    @JoinColumn(name = "material_id", nullable = false)
+    private co.edu.uceva.buildcheck.modules.materiales.model.Material material;
+
+    @Column(name = "usuario_id")
+    private Long usuarioId;
+
+    private String evidenciaFotografica;
+}
---
+++ build-check-backend/src/main/java/co/edu/uceva/buildcheck/modules/movimientos/repository/MovimientoRepository.java
@@ -0,0 +1,10 @@
+package main.java.co.edu.uceva.buildcheck.modules.movimientos.repository;
+
+import co.edu.uceva.buildcheck.modules.movimientos.model.Movimiento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovimientoRepository extends JpaRepository<Movimiento, Long> {
+
+}

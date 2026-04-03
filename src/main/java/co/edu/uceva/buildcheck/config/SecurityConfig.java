cad7817fd39f876fff5f6fbc273cb0325c6c719e
YermanAndress <geranroca1120@gmail.com>
2026-04-02 22:23:10 -0500

    BC-29 Feature: Añadir CRUD de usuarios, con validaciones de correo, encriptacion de contraseña, lista de roles disponibles

---
+++ src/main/java/co/edu/uceva/buildcheck/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package co.edu.uceva.buildcheck.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
---
+++ src/main/java/co/edu/uceva/buildcheck/modules/usuarios/model/Roles.java
@@ -0,0 +1,15 @@
+package co.edu.uceva.buildcheck.modules.usuarios.model;
+
+public class Roles {
+    public enum RolNombre {
+
+        ROLE_ADMIN,
+
+        ROLE_ALMACENISTA,
+
+        ROLE_DIRECTOR_OBRA,
+
+        ROLE_RESIDENTE
+
+    }
+}

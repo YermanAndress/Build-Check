8c0d91d8eaedb4e51f19c58b5708a1cbb91c5c75
YermanAndress <geranroca1120@gmail.com>
2026-05-27 15:25:00 -0500

    BC-31 Feature: Añadir DockerFile Y Docker Compose

--- src/main/java/co/edu/uceva/buildcheck/BuildCheckApplication.java
+++ src/main/java/co/edu/uceva/buildcheck/BuildCheckApplication.java
@@ -8,11 +8,21 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class BuildCheckApplication {
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
         // Cargar el archivo .env
-        Dotenv dotenv = Dotenv.load();
+        // Dotenv dotenv = Dotenv.load();
 
         // Pasar cada variable del .env al sistema de Java
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        dotenv.entries().forEach(entry -> {
+            if (System.getProperty(entry.getKey()) == null &&
+                    System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
         SpringApplication.run(BuildCheckApplication.class, args);
     }
 }
